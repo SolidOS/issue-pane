4226aff846b0dfe5f4d533e6d78c2983e2c64fc3
bourgeoa <alain.bourgeois10@gmail.com>
2025-02-04 17:38:48 +0100

    eslint config

---
+++ eslint.config.mjs
@@ -0,0 +1,35 @@
+import { fixupConfigRules } from "@eslint/compat";
+import globals from "globals";
+import path from "node:path";
+import { fileURLToPath } from "node:url";
+import js from "@eslint/js";
+import { FlatCompat } from "@eslint/eslintrc";
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+const compat = new FlatCompat({
+    baseDirectory: __dirname,
+    recommendedConfig: js.configs.recommended,
+    allConfig: js.configs.all
+});
+
+export default [
+    ...fixupConfigRules(compat.extends("eslint:recommended", "plugin:import/recommended")),
+    {
+        languageOptions: {
+            globals: {
+                ...globals.browser,
+                ...globals.node,
+                Atomics: "readonly",
+                SharedArrayBuffer: "readonly",
+            },
+        },
+
+        rules: {
+            "no-unused-vars": ["warn", {
+                argsIgnorePattern: "^_",
+                varsIgnorePattern: "^_",
+            }],
+        },
+    },
+];
