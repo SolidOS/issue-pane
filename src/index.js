ae003ebf299d2eda8a4a116ec10ba18219425b09
Sharon Stratsianis <sstratsianis@gmail.com>
2026-03-16 19:03:26 +1100

    resolved issue not popping up

--- src/index.js
+++ src/index.js
@@ -28,7 +28,7 @@ const kb = store
 // const TRACKER_ICON = UI.icons.iconBase + 'noun_list_638112'
 // const TASK_ICON = UI.icons.iconBase + 'noun_17020.svg'
 
-const OVERFLOW_CLASS = 'trackerIssuePaneOverflow'
+const OVERFLOW_CLASS = 'trackerOverlay'
 
 export default {
   icon: icons.iconBase + 'noun_122196.svg', // was: js/panes/issue/tbl-bug-22.png
@@ -572,7 +572,6 @@ export default {
     const overlay = paneDiv.appendChild(dom.createElement('div'))
     overlay.classList.add(OVERFLOW_CLASS)
     context.overlay = overlay
-    overlay.style.visibility = 'hidden'
 
     authn.checkUser().then(webId => {
       if (webId) {
